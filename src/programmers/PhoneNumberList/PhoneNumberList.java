a180f27a1be0df54244a3774384e178e20b8d09c
hyunji-ok <hyunjiok10@gmail.com>
2023-03-05 19:49:05 +0900

    [feat] 전화번호 목록 문제 풀이

---
+++ src/programmers/PhoneNumberList/PhoneNumberList.java
@@ -0,0 +1,37 @@
+package programmers.PhoneNumberList;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneNumberList {
+    public static void main(String[] args) {
+//        String[] phone_book = new String[] {"119", "97674223", "1195524421"};
+//        String[] phone_book = new String[] {"123","456","789"};
+        String[] phone_book = new String[] {"12","123","1235","567","88"};
+
+        PhoneNumberListSolution sol = new PhoneNumberListSolution();
+
+        System.out.println(sol.solution(phone_book));
+    }
+
+
+}
+
+class PhoneNumberListSolution {
+    public boolean solution(String[] phone_book) {
+        Map<String, Integer> map = new HashMap<>();
+
+        Arrays.stream(phone_book).forEach(i -> map.put(i, 1));
+
+        for (String s : phone_book) {
+            for (int j = 0; j < s.length(); j++) {
+                if (map.containsKey(s.substring(0, j))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/programmers/PhoneNumberList/README.md
@@ -0,0 +1,12 @@
+# 전화번호 목록
+
+## 문제 출처
+https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java
+
+## 느낀점
+굳이 해시를 써야되나 싶었는데, 효율성 테스트를 통과하기 위해서는 해시를 써야만 하는 문제였다.
+    
+    
+    
+    
+    
