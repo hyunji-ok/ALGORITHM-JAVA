7a277e78cbaf04c80da5c05acb2f32c168dbdcc3
hyunji-ok <hyunjiok10@gmail.com>
2023-03-05 20:45:02 +0900

    [feat] 위장 문제 풀이

---
+++ src/programmers/Camouflage/Camouflage.java
@@ -0,0 +1,49 @@
+package programmers.Camouflage;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Camouflage {
+
+    public static void main(String[] args) {
+        CamouflageSolution sol = new CamouflageSolution();
+
+        String[][] clothes = new String[][] {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+//        String[][] clothes = new String[][] {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+
+        System.out.println(sol.solution(clothes));
+    }
+}
+
+class CamouflageSolution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+
+        Arrays.stream(clothes).forEach(i->{
+            if(map.containsKey(i[1])){
+                Integer value = map.get(i[1])+1;
+                map.put(i[1], value);
+            } else {
+                map.put(i[1], 1);
+            }
+        });
+
+        if(map.keySet().size()>1){
+            return map.values().stream().reduce(1, (x,y)->(x)*(y+1))-1;
+        } else {
+            return (Integer) map.values().toArray()[0];
+        }
+    }
+
+//    public int solution(String[][] clothes) {
+//        return Arrays.stream(clothes)
+//                .collect(Collectors.groupingBy(p -> p[1], Collectors.mapping(p->p[0], Collectors.counting())))
+//                .values()
+//                .stream()
+////                .peek(System.out::println)
+//                .collect(Collectors.reducing(1L, (x,y) -> x*(y+1))).intValue()-1;
+//
+//    }
+}
---
+++ src/programmers/Camouflage/README.md
@@ -0,0 +1,13 @@
+# 위장
+
+
+## 문제 출처
+https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java
+
+## 느낀점
+스트림에 어느 정도 익숙해졌다 싶었는데, collect와 collectors를 안 쓰고 반쪽짜리 상태였음을 깨달았다.
+    
+    
+    
+    
+    
