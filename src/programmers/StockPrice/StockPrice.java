1dfc76a67af59c72a97768db533254ff2724bfc4
hyunji-ok <hyunjiok10@gmail.com>
2023-11-29 14:23:10 +0900

    [feat] 주식가격 문제 해결

---
+++ src/programmers/StockPrice/StockPrice.java
@@ -0,0 +1,46 @@
+package programmers.StockPrice;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StockPrice {
+    public static void main(String[] args) {
+        StockPriceSolution sol = new StockPriceSolution();
+
+        int[] prices = {1, 2, 3, 2, 3};
+        int[] answer = {4, 3, 1, 1, 0};
+
+        int[] realSol = sol.solution(prices);
+
+        System.out.println(Arrays.toString(realSol));
+        System.out.println(Arrays.equals(realSol, answer));
+
+    }
+
+}
+
+class StockPriceSolution {
+    public int[] solution(int[] prices) {
+        Queue<Integer> answerQue = new LinkedList<>();
+        int cnt = 0;
+
+        for(int i=0; i< prices.length; i++){
+            cnt = 0;
+            for(int j=i+1; j< prices.length+1; j++){
+                if(j == prices.length) {
+                    answerQue.offer(cnt);
+                    break;
+                }
+
+                cnt++;
+                if(prices[i] > prices[j]) {
+                    answerQue.offer(cnt);
+                    break;
+                }
+            }
+        }
+
+        return answerQue.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
