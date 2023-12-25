fdfa1d491af65d98947696778fa2b403cb38e0aa
hyunji-ok <hyunjiok10@gmail.com>
2023-12-25 20:02:52 +0900

    [feat] 완주하지 못한 선수 자바스크립트 버전 해결

---
+++ src/programmers/AthletesWhoDidNotFinish/javascript/AthletesWhoDidNotFinish.js
@@ -0,0 +1,30 @@
+function test(){
+    const participant = ["leo", "kiki", "eden"]
+    const completion = ["eden", "kiki"]
+
+    const expAns= "leo"
+    const realAns= solution(participant, completion)
+
+    console.log(realAns)
+    console.log(realAns===expAns)
+
+}
+
+test();
+
+function solution(participant, completion) {
+    let hash = {}
+    participant.forEach((par) => {
+        hash[par] = hash[par] ? hash[par] +1 : 1
+    })
+    completion.forEach((par) => {
+        hash[par] = hash[par] -1
+    })
+
+    for(let key in hash) {
+        if(hash[key] >= 1) {
+            return key
+        }
+    }
+
+}
